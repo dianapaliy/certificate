370e2f12573aaa89407939e403a339ea48e2263d
Diana Paliy <paliy.diana@gmail.com>
2015-04-02 18:55:22 +0300

    first commit, header for desktop

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+    // Конфигурация проекта
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        //------------------------------------------------------------
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions', 'ie 9']
+            },
+            dev: {
+                src: 'css/all.css'
+            }
+        },
+        less: { 
+            options: {
+                expand: true,
+                sourceMap: true
+            },
+            dev: {
+                files: {
+                    'css/all.css': ['less/all.less']
+                }
+            }
+        },
+        watch: {
+            less: {
+                files: ['less/**/*.less'],
+                tasks: ['less', 'autoprefixer']
+            }
+        },
+        browserSync: {
+            files: {
+                src: ['*.html', 'css/all.css']
+            },
+            options: {
+                watchTask: true,
+                server: {
+                    baseDir: '.'
+                }
+            }
+        }
+    });
+    require('load-grunt-tasks')(grunt);
+    grunt.registerTask('default', ['less', 'autoprefixer', 'browserSync', 'watch']);
+};
