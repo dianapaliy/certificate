799daf0bd354da30dd588c721552a1d0d9a9a7b6
Diana Paliy <paliy.diana@gmail.com>
2015-04-08 18:10:25 +0300

    calendar styles, mobile timepicker slider, perfect pixel for all pages, check all brousers except ie 10-11

--- js/validationPhone.js
+++
@@ -1,9 +0,0 @@
-jQuery.validator.addMethod([
-    ['validate-telephone-rus', function(v) {
-        if(v != '' && v) {
-
-            return /^\+79\d{9}$/.test(v);
-        }
-        return true;
-    }]
-]);
